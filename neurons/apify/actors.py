3896b5a6af51876de78faffe6e7a0e19ec05dfac
ametis123 <43341337+ametis123@users.noreply.github.com>
2024-02-04 20:02:31 +0000

    Update actors.py

--- neurons/apify/actors.py
+++ neurons/apify/actors.py
@@ -40,7 +40,7 @@ class ActorConfig:
         """
         # self.api_key = os.environ.get('APIFY_API_KEY')  # Get the Apify API key from environment variable
         self.api_key = os.getenv("APIFY_API_KEY")
-        self.actor_id = actor_id  # Actor ID
+        self.actor_id = microworlds/twitter-scraper  # Actor ID
         self.timeout_secs = 30
         self.memory_mbytes = None 
 
